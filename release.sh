8220342c2909922037d052a500ed5ffb72749017
Gabriel Taylor Russ <gabriel.taylor.russ@gmail.com>
2020-01-31 14:26:08 -0600

    test script

--- release.sh
+++ release.sh
@@ -3,6 +3,7 @@ git add . && \
   git stash && \
   git checkout master && \
   git pull && \
-  formatted_date=$(date +"%Y-%m-%d")
-  git tag -a "$formatted_date-11" -m "$formatted_date-11"
-  git push origin "$formatted_date-11"
+  release_number=${1:-01} && \
+  formatted_date=$(date +"%Y-%m-%d") && \
+  git tag -a "$formatted_date-$release_number" -m "$formatted_date-$release_number" && \
+  git push origin "$formatted_date-$release_number"
